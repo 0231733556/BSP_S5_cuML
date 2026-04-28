742fa15cbf1b5b06e13203ed062e1295ed86c2fb
Kylian <0231733556@uni.lu>
2026-04-28 22:08:09 +0200

    feat: evaluated server performance

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
-ACCELERATORS = ["CPU","GPU1", "S2-CPU","S2-GPU"]
+ACCELERATORS = ["CPU","GPU1", "S2-CPU","S2-GPU","test1"]
 CLASSIFIERS = ["svc", "random_forest","logistic"]
 CLUSTERERS = ["kmeans", "dbscan","agglomerative"]
 SEED=42
